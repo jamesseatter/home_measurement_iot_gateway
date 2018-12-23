ec1e942a11272f8650a01546c5056e01ded4f0f9
james seatter <james.seatter@hotmail.co.uk>
2018-12-23 19:50:05 +0100

    Initial Project

---
+++ src/main/java/eu/seatter/homeheating/edge/HomeHeatingIotEdgeApplication.java
@@ -0,0 +1,14 @@
+package eu.seatter.homeheating.edge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeHeatingIotEdgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeHeatingIotEdgeApplication.class, args);
+	}
+
+}
+
