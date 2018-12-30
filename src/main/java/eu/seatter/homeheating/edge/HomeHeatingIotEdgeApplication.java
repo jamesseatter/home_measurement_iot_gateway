a4178a5537960d1589860f1e361ececa371fc779
james seatter <james.seatter@hotmail.co.uk>
2018-12-30 18:07:42 +0100

    Initialise Project, Spring WebFlux. Closed #1 Configure H2. Closed #5 Add lombok. Closed #7 Add spring actuator. Closed #8 Add basic Index.html to resources/templates. Closed #9

--- src/main/java/eu/seatter/homeheating/edge/HomeHeatingIotEdgeApplication.java
+++ src/main/java/eu/seatter/homeheating/edge/HomeHeatingIotEdgeApplication.java
@@ -1,13 +1,17 @@
 package eu.seatter.homeheating.edge;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class HomeHeatingIotEdgeApplication {
 
 	public static void main(String[] args) {
+		log.info("Application startup");
 		SpringApplication.run(HomeHeatingIotEdgeApplication.class, args);
+		log.info("Application shutdown");
 	}
 
 }
